bfecdd0dbd4953e3a19532a292c700bb55534aeb
Danik <elik@example.com>
2023-12-09 19:22:06 +0300

    second coimmit for check

---
+++ src/test/java/portfolio/webApplications/WebApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.webApplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
