7d2c63230ccee3c57a12244ad24fee59ab8b4ec7
Nick Morin <morinnick31@gmail.com>
2023-12-17 16:55:20 +0300

    add about block

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header/Header";
+import About from "./components/about/About";
 import "./app.css";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <About></About>
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,14 @@
+import "./about.css";
+const About = () => {
+  return (
+    <section class="about">
+      <h2>about us</h2>
+      <p>
+        WE make web - applications for your business. It’s increases your sales
+        and business getting stronger and bigger, better what was .
+      </p>
+    </section>
+  );
+};
+
+export default About;
