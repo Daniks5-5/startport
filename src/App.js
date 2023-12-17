e0fd3aff6c9856339d88487d1fd4090ac5a092e9
Nick Morin <morinnick31@gmail.com>
2023-12-17 17:46:51 +0300

    add skills block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/header/Header";
 import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
 import "./app.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <About></About>
+      <Skills></Skills>
     </div>
   );
 }
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,27 @@
+// import "../../app.css";
+import "./skills.css";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <div className="skills-font"></div>
+      <div className="skills-block">
+        <h2>Our skills</h2>
+        <ul className="skills-list">
+          <li>Java 17+</li>
+          <li>Spring Boot 2+</li>
+          <li>PostgreSQL</li>
+          <li>JavaScript</li>
+          <li>WordPress</li>
+          <li>User Interface (UI) Design</li>
+          <li>Rest</li>
+          <li>Docker</li>
+          <li>Html5/CSS</li>
+          <li>1-С Битрикс</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
